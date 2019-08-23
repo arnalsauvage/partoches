2f90e59ea95470efef2b15135b778b1078f695c6
Arnaud Medina <arnaudmedina@gmail.com>
2019-08-24 00:41:17 +0200

    Page brouillon pour lister les chansons préférées d'un groupe : mémoriser les coches, afficher la liste après le formulaire

--- php/utilisateurBand_form.php
+++ php/utilisateurBand_form.php
@@ -9,27 +9,7 @@ $utilisateurVoir = "utilisateur_voir.php";
 $table = "utilisateur";
 $affichage = "";
 
-// Gestion de la form
-if (isset($_POST['utilisateur'])) {
-    $listeUtilisateurs = "";
-/*    foreach ($_POST['utilisateur'] as $valeur) {
-        echo "La checkbox utilisateur $valeur a été cochée<br>";
-    }
- */
-    $listeUtilisateurs .= implode(",", $_POST['utilisateur']);
-
-    $marequete = "select chanson.nom , SUM(noteUtilisateur.note) as score from chanson, noteUtilisateur
-            where chanson.id = noteUtilisateur.idObjet and noteUtilisateur.nomObjet='chanson'
-            AND noteUtilisateur.idUtilisateur in ($listeUtilisateurs)
-            GROUP BY chanson.nom ORDER BY score DESC";
-    $resultat = $_SESSION ['mysql']->query($marequete);
-
-    while ($ligne = $resultat->fetch_row()) {
-        $affichage .= $ligne[0] . " ";
-        $affichage .= $ligne[1] . "<br>";
-    }
-}
-
+$listeUtilisateurs = implode(",", $_POST['utilisateur']);
 
 $affichage .= entreBalise("Utilisateurs", "H1");
 $affichage .= "<form action=\"utilisateurBand_form.php\" method=\"post\"
@@ -44,21 +24,42 @@ if (!$resultat)
 $numligne = 0;
 
 // Affichage de la liste
-
 while ($ligne = $resultat->fetch_row()) {
     $numligne++;
         $affichage .= TblDebutLigne();
 
         $affichage .= "<div>
-        <input type='checkbox' id='". $ligne[0] ."' value='". $ligne[0] ."' name='utilisateur[]'
-         >
+        <input type='checkbox' id='". $ligne[0] ."' value='". $ligne[0] ."' name='utilisateur[]'";
+        if (strchr($listeUtilisateurs,$ligne[0]))
+            $affichage .= " checked ";
+        $affichage .=">
         <label for='". $ligne[0] ."'>";
         $affichage .= TblCellule($ligne [3] . " " . $ligne [4]); // nom prenom
         $affichage .= "</label></div><br>\n";
-
-
 }
-$affichage .= " <input type=\"submit\" value=\"Envoyer\" /></form>";
+$affichage .= " <input type=\"submit\" value=\"Envoyer\" /></form> <br>";
+
+// Gestion de la form
+if (isset($_POST['utilisateur'])) {
+    /*    foreach ($_POST['utilisateur'] as $valeur) {
+            echo "La checkbox utilisateur $valeur a été cochée<br>";
+        }
+     */
+
+    $marequete = "select chanson.nom , SUM(noteUtilisateur.note) as score from chanson, noteUtilisateur
+            where chanson.id = noteUtilisateur.idObjet and noteUtilisateur.nomObjet='chanson'
+            AND noteUtilisateur.idUtilisateur in ($listeUtilisateurs)
+            GROUP BY chanson.nom ORDER BY score DESC";
+    //$affichage .= "Requete :" . $marequete;
+    $resultat = $_SESSION ['mysql']->query($marequete);
+
+    if ($resultat) {
+        while ($ligne = $resultat->fetch_row()) {
+            $affichage .= $ligne[0] . " ";
+            $affichage .= $ligne[1] . "<br>";
+        }
+    }
+}
 
 $affichage .= envoieFooter();
 echo $affichage;
