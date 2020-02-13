12c116c7b1eb8b7ef2bc49e8336277932ce471da
Arnaud Medina <arnaudmedina@gmail.com>
2020-02-13 19:24:17 +0100

    Ajout d'un filtre dans la page documents : filtre les mp3, les pdf ou les doc

--- php/documents_voir.php
+++ php/documents_voir.php
@@ -51,7 +51,7 @@ $tabUsers = portraitDesUtilisateurs();
 if (isset ($_POST['filtre'])) {
     $contenuFiltrer = $_POST['filtre'];
     $contenuFiltrer = htmlspecialchars($contenuFiltrer, ENT_QUOTES);
-    echo "filtre :" . $contenuFiltrer;
+    //echo "filtre :" . $contenuFiltrer;
 } else {
     $contenuFiltrer = "";
 }
@@ -95,17 +95,16 @@ while ($ligneDoc = $lignes->fetch_row()) {
     $fichierCourt = composeNomVersion($ligneDoc [1], $ligneDoc [4]);
     $fichier = "../data/chansons/" . $ligneDoc [6] . "/" . composeNomVersion($ligneDoc [1], $ligneDoc [4]);
     $extension = substr(strrchr($ligneDoc [1], '.'), 1);
+    echo "extension " . $extension . " et filtre : " . $contenuFiltrer . " - " ;
 
-
-    if ($_POST['filtrer'])
-    {
-        if (($contenuFiltrer=="son")&&($extension<>"mp3")){
+    if ($contenuFiltrer) {
+        if (($contenuFiltrer == "son") && ($extension <> "mp3")) {
             continue;
         }
-        if (($contenuFiltrer=="pdf")&&($extension<>"pdf")){
+        if (($contenuFiltrer == "pdf") && ($extension <> "pdf")) {
             continue;
         }
-        if (($contenuFiltrer=="doc")&&($extension<>"doc")){
+        if (($contenuFiltrer == "doc") && ($extension <> "doc")) {
             continue;
         }
     }
@@ -115,25 +114,25 @@ while ($ligneDoc = $lignes->fetch_row()) {
     if (!file_exists("../images/icones/" . $extension . ".png")) {
         $icone = Image("../images/icones/fichier.png", 32, 32, "icone");
     }
-    $precedenteVignette = $vignetteChanson;
-    $vignettePublicateur = Image("../images" . $tabUsers [$ligneDoc [7]] [1], 48, 48, $tabUsers [$ligneDoc [7]] [0]);
-    $sortie .= "<td> $vignettePublicateur </td>\n";
-    $vignetteChanson = Image("../data/chansons/" . $ligneDoc [6] . "/" . imageTableId(CHANSON, $ligneDoc [6]), 128, 128, CHANSON);
-    if ($precedenteVignette != $vignetteChanson) {
-        $sortie .= "<td> $vignetteChanson </td>\n";
-    } else {
-        $sortie .= "<td>  </td>\n ";
-    }
-    $sortie .= "<td> " . Ancre("getdoc.php?doc=" . $ligneDoc [0], $icone, "", true) . "<a href= '" . $fichier . "' target='_blank'> " . $fichierCourt . "</a> \n";
-    $sortie .= "<td>" . intval($ligneDoc [2] / 1024) . " ko  </td>";
-    $sortie .= "<td>" . " -  publié le " . dateMysqlVersTexte($ligneDoc [3]) . " </td>";
-    $sortie .= "<td> &nbsp - " . $ligneDoc [8] . " vues </td></tr>\n";
+        $precedenteVignette = $vignetteChanson;
+        $vignettePublicateur = Image("../images" . $tabUsers [$ligneDoc [7]] [1], 48, 48, $tabUsers [$ligneDoc [7]] [0]);
+        $sortie .= "<td> $vignettePublicateur </td>\n";
+        $vignetteChanson = Image("../data/chansons/" . $ligneDoc [6] . "/" . imageTableId(CHANSON, $ligneDoc [6]), 128, 128, CHANSON);
+        if ($precedenteVignette != $vignetteChanson) {
+            $sortie .= "<td> $vignetteChanson </td>\n";
+        } else {
+            $sortie .= "<td>  </td>\n ";
+        }
+        $sortie .= "<td> " . Ancre("getdoc.php?doc=" . $ligneDoc [0], $icone, "", true) . "<a href= '" . $fichier . "' target='_blank'> " . $fichierCourt . "</a> \n";
+        $sortie .= "<td>" . intval($ligneDoc [2] / 1024) . " ko  </td>";
+        $sortie .= "<td>" . " -  publié le " . dateMysqlVersTexte($ligneDoc [3]) . " </td>";
+        $sortie .= "<td> &nbsp - " . $ligneDoc [8] . " vues </td></tr>\n";
 }
-
 $sortie .= "</table>";
 $sortie .= $pagination->barrePagination() . "   ";
 $sortie .= envoieFooter();
 echo $sortie;
+
 function titreColonne($libelle, $nomRubrique)
 {
     return TblCellule(Ancre("?tri=$nomRubrique", "<span class='glyphicon glyphicon-chevron-up'> ") . "  $libelle   " . Ancre("?triDesc=$nomRubrique", "  <span class='glyphicon glyphicon-chevron-down'> "));
