6de834ebc6f5f01334ba705c0fc10a7ab8432cf3
arnalsauvage <arnaudmedina@gmail.com>
2021-04-17 11:42:38 +0200

    Mise au propre du html de songbook_liste

--- php/songbook_liste.php
+++ php/songbook_liste.php
@@ -1,13 +1,23 @@
 <?php
-include_once("lib/utilssi.php");
+
+use JetBrains\PhpStorm\Pure;
+
 include_once("menu.php");
 include_once("songbook.php");
 include_once("document.php");
+include_once("/lib/utilssi.php");
+
+global $cheminImages;
+global $iconePoubelle;
+global $songbookForm;
+global $iconeCreer;
+global $cheminImagesSongbook;
+global $songbookGet;
+global $songbookVoir;
 
 $table = "songbook";
 $fichiersDuSongbook = "";
 $fichiersDuSongbook .= entreBalise("Songbooks", "H1");
-$fichiersDuSongbook .= TblDebut(0);
 
 // Gestion du paramètre de tri
 if (isset ($_GET ['tri'])) {
@@ -30,20 +40,19 @@ $numligne = 0;
 // Affichage de la liste
 
 // //////////////////////////////////////////////////////////////////////ADMIN : bouton nouveau
-if ($_SESSION ['privilege'] >= 2)
+if ($_SESSION ['privilege'] >= 2) {
     $fichiersDuSongbook .= "<BR>" . Ancre("$songbookForm", Image($cheminImages . $iconeCreer, 32, 32) . "Créer un nouveau songbook");
+}
 // //////////////////////////////////////////////////////////////////////ADMIN
 
-$fichiersDuSongbook .= Image($iconeAttention, "100%", 1, 1);
-$fichiersDuSongbook .= TblDebut(0);
-$fichiersDuSongbook .= TblCellule(Ancre("?tri=hits", "Hits")) . TblFinLigne();
-
-$fichiersDuSongbook .= TblDebut(0);
+$fichiersDuSongbook .= TblDebut();
 $fichiersDuSongbook .= TblDebutLigne() . TblCellule("  Tri  ");
 $fichiersDuSongbook .= titreColonne("Nom", "nom");
 $fichiersDuSongbook .= titreColonne("Description", "description");
 $fichiersDuSongbook .= titreColonne("Date", "date");
 $fichiersDuSongbook .= titreColonne("Vues", "hits");
+$fichiersDuSongbook .= titreColonne("", "");
+$fichiersDuSongbook .= titreColonne("", "");
 $fichiersDuSongbook .= TblFinLigne();
 
 while ($ligne = $resultat->fetch_row()) {
@@ -52,17 +61,22 @@ while ($ligne = $resultat->fetch_row()) {
     // Songbook : 	[0]id 	[1]nom 	[2]description 	[3]date  	[4]image 	[5]hits 	[6]idUser
     if ($ligne [4])
         // //////////////////////////////////////////////////////////////////////ADMIN : bouton modifier
-        if ($_SESSION ['privilege'] >= 2)
-            $fichiersDuSongbook .= TblCellule(Ancre($songbookForm . "?id=$ligne[0]", Image(($cheminImagesSongbook . $ligne[0] . "/" . urlencode(imageTableId("songbook", $ligne[0]))), 32, 32, "couverture"))); // image
-        else
-            $fichiersDuSongbook .= TblCellule(Image(($cheminImagesSongbook . $ligne[0]) . "/" . urlencode(imageTableId("songbook", $ligne[0])), 32, 32)); // image
-    else
+    {
+        if ($_SESSION ['privilege'] >= 2) {
+            $fichiersDuSongbook .= TblCellule(Ancre($songbookForm . "?id=$ligne[0]", Image(($cheminImagesSongbook . $ligne[0] . "/" . urlencode(imageTableId("songbook", $ligne[0]))), 32, 32, "couverture")));
+        } // image
+        else {
+            $fichiersDuSongbook .= TblCellule(Image(($cheminImagesSongbook . $ligne[0]) . "/" . urlencode(imageTableId("songbook", $ligne[0])), 32, 32));
+        }
+    } // image
+    else {
         $fichiersDuSongbook .= TblCellule(Ancre($songbookForm . "?id=$ligne[0]", "voir"));
+    }
 
     $fichiersDuSongbook .= TblCellule(Ancre($songbookVoir . "?id=$ligne[0]", entreBalise($ligne [1], "H2"))); // Nom
 
     $fichiersDuSongbook .= TblCellule("  " . $ligne [2]); // description
-    $fichiersDuSongbook .= TblCellule(" " . dateMysqlVersTexte($ligne [3], 0)); // date
+    $fichiersDuSongbook .= TblCellule(" " . dateMysqlVersTexte($ligne [3])); // date
     $fichiersDuSongbook .= TblCellule("  -  " . $ligne [5] . " hit(s)"); // hits
 
     // //////////////////////////////////////////////////////////////////////ADMIN : bouton supprimer
@@ -76,16 +90,16 @@ while ($ligne = $resultat->fetch_row()) {
 }
 $fichiersDuSongbook .= TblFin();
 
-$fichiersDuSongbook .= Image($iconeAttention, "100%", 1, 1);
 // //////////////////////////////////////////////////////////////////////ADMIN : bouton ajouter
-if ($_SESSION ['privilege'] >= 2)
+if ($_SESSION ['privilege'] >= 2) {
     $fichiersDuSongbook .= "<BR>" . Ancre("?page=$songbookForm", Image($cheminImages . $iconeCreer, 32, 32) . "Créer un nouvel songbook");
+}
 // //////////////////////////////////////////////////////////////////////ADMIN
 $fichiersDuSongbook .= envoieFooter();
 echo $fichiersDuSongbook;
 
-function titreColonne($libelle, $nomRubrique)
+#[Pure] function titreColonne($libelle, $nomRubrique): string
 {
-    $chaine = TblCellule(Ancre("?tri=$nomRubrique", "<span class='glyphicon glyphicon-chevron-up'> ") . "  $libelle   " . Ancre("?triDesc=$nomRubrique", "  <span class='glyphicon glyphicon-chevron-down'> "));
-    return $chaine;
+    return TblCellule(Ancre("?tri=$nomRubrique", "<span class='glyphicon glyphicon-chevron-up'> </span>")
+        . "  $libelle   " . Ancre("?triDesc=$nomRubrique", "  <span class='glyphicon glyphicon-chevron-down'></span> "));
 }
