378b1315854b468661c4450339084f485e49cb00
arnalsauvage <arnaudmedina@gmail.com>
2022-01-28 22:11:30 +0100

    Correctifs

--- php/utilisateurBand_form.php
+++ php/utilisateurBand_form.php
@@ -9,8 +9,8 @@ $utilisateurVoir = "utilisateur_voir.php";
 $table = "utilisateur";
 $affichage = "";
 
-if (isset($_POST['utilisateur'])) {
-    $listeUtilisateurs = implode(",", $_POST['utilisateur']);
+if (isset($_POST['utilisateurs'])) {
+    $listeUtilisateurs = implode(",", $_POST['utilisateurs']);
 }
 
 $affichage .= entreBalise("Utilisateurs", "H1");
@@ -22,34 +22,40 @@ $affichage .= "<form action=\"utilisateurBand_form.php\" method=\"post\"
 // Chargement de la liste des utilisateurs et de leur nombre de votes
 
 $marequete = "select u.id , u.prenom, u.nom ,
-(select count(*) from noteutilisateur nu WHERE nu.idUtilisateur = u.id)  votes
+(select count(*) from noteUtilisateur nu WHERE nu.idUtilisateur = u.id)  votes
 from utilisateur u
 order by votes DESC";
 $resultat = $_SESSION ['mysql']->query($marequete);
-if (!$resultat)
-    die ("Problème utilisateursListe #1 : " . $_SESSION ['mysql']->error);
-$numligne = 0;
+if (!$resultat){
+    echo "ma requete : $marequete";
+    die ("Problème utilisateursBand #1 : " . $_SESSION ['mysql']->error);
+}
 
 // Affichage de la liste
+$numligne = 0;
 while ($ligne = $resultat->fetch_row()) {
     $numligne++;
-        $affichage .= "<div>
-        <input type='checkbox' style = 'width: 20px;' id='". $ligne[0] ."' value='". $ligne[0] . "'";
-        if (isset($_POST['utilisateur']) && in_array($ligne[0],$_POST['utilisateur']))
+        $affichage .= "
+        <div>
+        <input type='checkbox' style = 'width: 20px;' id='". $ligne[0] ."' value='". $ligne[0] . "' ' name='utilisateurs[]'";
+        if (isset($_POST['utilisateurs']) && in_array($ligne[0],$_POST['utilisateurs'])) {
             $affichage .= " checked ";
-        $affichage .= ">
-        <label  style = 'width: 240px;'for='" . $ligne[0] ."'>";
+        }
+    $affichage .= ">
+        <label  style = 'width: 240px;' for='" . $ligne[0] ."'>";
         $affichage .= $ligne [1] . " " . $ligne [2] ." " . $ligne [3]  . " votes" ; // nom prenom
         $affichage .= "</label></div><br>\n";
 }
 $affichage .= " <input type=\"submit\" value=\"Envoyer\" /></form> <br>";
 
 // Gestion de la form
-if (isset($_POST['utilisateur'])) {
-    /*    foreach ($_POST['utilisateur'] as $valeur) {
-            echo "La checkbox utilisateur $valeur a été cochée<br>";
+if (isset($_POST['utilisateurs'])) {
+        if (true==false) {
+            foreach ($_POST['utilisateurs'] as $valeur) {
+                echo "La checkbox utilisateur $valeur a été cochée<br>";
+            }
         }
-     */
+    $nombreMembres = count($_POST['utilisateurs']);
 
     $marequete = "select chanson.id , chanson.nom , SUM(noteUtilisateur.note) as score from chanson, noteUtilisateur
             where chanson.id = noteUtilisateur.idObjet and noteUtilisateur.nomObjet='chanson'
@@ -57,13 +63,20 @@ if (isset($_POST['utilisateur'])) {
             GROUP BY chanson.id ORDER BY score DESC";
     //$affichage .= "Requete :" . $marequete;
     $resultat = $_SESSION ['mysql']->query($marequete);
-
+    if (!$resultat){
+        echo "ma requete : $marequete";
+        die ("Problème utilisateursBand #2 : " . $_SESSION ['mysql']->error);
+    }
     if ($resultat) {
         $_compteur = 1;
         while ($ligne = $resultat->fetch_row()) {
+            if (round($ligne[2]/$nombreMembres,2)<3)
+            {
+                break;
+            }
             $affichage .= $_compteur++ . " - ";
             $affichage .= $ligne[1] . " ";
-            $affichage .= $ligne[2] . "<br>";
+            $affichage .= round($ligne[2]/$nombreMembres,1) . "<br>";
         }
     }
 }
