0c48e3c8dfecf17696c918f11df8c3745c25fb58
arnalsauvage <arnaudmedina@gmail.com>
2024-01-19 08:37:46 +0100

    Ajout de la gestion des vidéos en mp4 affichées dans une balise <video>

--- php/chanson/chanson_voir.php
+++ php/chanson/chanson_voir.php
@@ -116,9 +116,11 @@ if ($result->num_rows > 0) {
         if (!file_exists(ICONES. $extension . ".png")) {
             $icone = Image("../images/icones/fichier.png", 32, 32, "icone");
         }
-        $contenuHtml .= "<a href= '" . lienUrlAffichageDocument($ligne [0]) . "' target='_blank'> $icone  <br>" .
-            htmlentities($fichierSec) . "</a> <br>\n";
-        $contenuHtml .= FIN_DIV;
+        if ($extension <> "mp3" && $extension <> "m4a" && $extension<> "mp4") {
+            $contenuHtml .= "<a href= '" . lienUrlAffichageDocument($ligne [0]) . "' target='_blank'> $icone  <br>" .
+                htmlentities($fichierSec) . "</a> <br>\n";
+            $contenuHtml .= FIN_DIV;
+        }
     }
     $contenuHtml .= FIN_SECTION . "\n";
 
