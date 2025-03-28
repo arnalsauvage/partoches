e78e60e55f18f9b3e3a27e4498ceb3ecca495233
arnalsauvage <arnaudmedina@gmail.com>
2025-03-28 01:02:09 +0100

    Refacto di code

--- php/utilisateur/utilisateurBand_form.php
+++ php/utilisateur/utilisateurBand_form.php
@@ -63,7 +63,7 @@ order by votes DESC";
         die ("Problème utilisateursBand #1 : " . $_SESSION ['mysql']->error);
     }
 // Affichage de la liste de selection des utilisateurs
-    $affichage .= ">";
+    $affichage .= "<form action=\"utilisateurBand_form.php\" method=\"post\" enctype=\"multipart/form-data\">";
     $numligne = 0;
     while ($ligne = $resultat->fetch_row()) {
         $numligne++;
