f6f5fec183d3b33f5947d8652d19517d207b8f1c
arnalsauvage <arnaudmedina@gmail.com>
2025-10-19 23:07:25 +0200

    MLise à jour de lafeuille de style

--- php/lib/html.php
+++ php/lib/html.php
@@ -187,7 +187,12 @@ if (!isset ($FichierHtml)) {
 		<meta charset='UTF-8' >";
 
         // Pour BootStrap
-        $retour .= " rel='stylesheet'>
+        $retour .= "
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+		<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    	<link href='../../css/bootstrap.min.css' rel='stylesheet'>
+        <!-- source : http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css -->
+    	 <link href=\"../../css/jquery-ui.1.12.1.css\" rel='stylesheet'>
         <link rel='stylesheet' type='text/css' href='../../css/styles.0.3.css'>
     	 ";
 
